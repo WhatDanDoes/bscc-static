a79541266dd9aebb36c96dcb8359649461d353ef
Daniel Bidulock <daniel@capitolhill.ca>
2022-11-30 09:25:34 -0700

    Copy through images and fonts

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ const blogTools = require('eleventy-plugin-blog-tools');
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('./src/css');
+  eleventyConfig.addPassthroughCopy('./src/fonts');
+  eleventyConfig.addPassthroughCopy('./src/images');
   eleventyConfig.addPassthroughCopy('./src/resources');
 
   eleventyConfig.addPlugin(blogTools);
