4ff21a19164f7e242b5338a312ddca53132aef87
Daniel Bidulock <daniel@capitolhill.ca>
2023-11-30 09:07:30 -0700

    Hello, matomo partial

--- src/_data/project.js
+++ src/_data/project.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 module.exports = function() {
   return {
     environment: process.env.NODE_ENV || '',
-    url: process.env.NODE_ENV === 'development' ? `dev.${process.env.VIRTUAL_HOST}` : process.env.VIRTUAL_HOST
+    url: process.env.NODE_ENV === 'development' ? `dev.${process.env.VIRTUAL_HOST}` : process.env.VIRTUAL_HOST,
+    matomo: process.env.MATOMO_URL,
+    matomoId: process.env.MATOMO_SITE_ID,
   };
 };
